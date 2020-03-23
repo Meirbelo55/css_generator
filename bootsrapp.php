6413c692b157a28e73273b19a4093db1de24c674
Meirbelo55 <62239240+Meirbelo55@users.noreply.github.com>
2020-03-23 11:18:19 +0100

    Add files via upload

--- bootsrapp.php
+++ bootsrapp.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<div class="image-2"></div>
+	</body>
+</html>
 <?php
 function my_merge_image($first_img_path = NULL, $second_img_path = NULL)
 		{
@@ -17,60 +27,45 @@ function my_merge_image($first_img_path = NULL, $second_img_path = NULL)
 							imagecopy($destination, $source1, 0, 0, 0, 0, imagesx($source1), imagesy($source1));
 							//concatene les 2images
 							imagecopy($destination, $source2, imagesx($source1), 0,  0,  0,  imagesx($source2),imagesx($source2));
-							//header('content-type: image/png');
 							imagepng($destination, 'imagedefine.png');
               //sauvegarde l image sur le disque dur
-								 	if (imagesx($source1) > imagesx($source2))
-									 	{
-									 		//echo 'c bon';
-									 	}
-										 elseif(imagesx($source1) != imagesx($source2))
-										 	{
-										 		//echo 'mdrrrr';
-							        }
 												imagedestroy($destination);
 												imagedestroy($source1);
 		}
 		//apel de fonction
 my_merge_image('mes_images/image1.png', 'mes_images/image2.png');
 
-function my_generate_css($name)
+function my_generate_css($array)
 {
-	$open = opendir($name);
-	// $entre = readdir($open);
+	$i = 0;
 	$openfil = fopen('generator.css','w');
-	while($jelis = readdir($open))
+	foreach ($array  as  $value)
 	{
-		//echo $jelis;
-		if ($jelis !== '.' && $jelis !== '..')
-		{
-		  $rest = substr($jelis, 0, -4);
-		    $css =  '.'.$rest.'
-			{
-			width: 40px;
-			height: 50px;
-		}';
+		 $getimg = getimagesize($value);
+		 $x = $getimg[0];
+		 $resultat = substr($value, 13, -4);
+		 	    $css =  '.'.$resultat.'
+		 		{
+		 			background:url(mes_images/sprite.png) -'.$i.'px  -0px ;
 
-			file_put_contents('generator.css',$css,FILE_APPEND);'\n';
-	 }
-	'\n';
-//$css = 'left : 50px';
-}
-}
-my_generate_css('mes_images');
+					width: '.$getimg[0].'px;
+					height : '.$getimg[1].'px;
+		 		}'."\n";
+				file_put_contents('generator.css',$css,FILE_APPEND);'\n';
+				$i += $x;
+ 	}
+}'\n'
+;
+
 
 function my_scandir($dir_path)
 {
 	 $dir = opendir($dir_path);
-	// echo $dir;
 	 $come = readdir($dir);
-	 //echo $dir;
 	if (pathinfo($dir_path,PATHINFO_EXTENSION) == 'png') {
 		readdir($pathinfo);
-	//	echo pathinfo($dir_path);
 	}
 }
-//my_scandir('mes_images');
  $openadir = opendir('./mes_images');
 $myarray = [];
 function recursive($longeur=1,$largeur)
@@ -82,55 +77,33 @@ $mondossier = opendir('./mes_images');
   {
     if (pathinfo($lire,PATHINFO_EXTENSION) !== 'png')
        {
-          //echo 'alert!!!!!! le sprite peut se faire q"avec les extensions png . <br>';
+
        }
         else
            {
 						array_push($myarray,'./mes_images/'.$lire);
 						$imgcopy = imagecreatefrompng('./mes_images/'.$lire);
-						echo $lire;
+						 $lire;
 
            }
     }
 		if ($lire !== '.' && $lire !== '..') {
-		 //$array =  array($lire . '<br>');
-		 //echo $lire.'<br>';
-		 //var_dump($imgcopy).'<br>';
-		 //$dest = imagecreatetruecolor(imagesx($imgcopy),imagesy($imgcopy));
-		 //imagecopy($dest,$imgcopy,0,0,0,0,128,128);
-
-		 // echo $x;
-		 // echo $y;
-		 //var_dump($myarray);
 		 $i = 0;
 		 $u = 0;
-		 $desti = imagecreatetruecolor(2000,2000);
+		 $desti = imagecreatetruecolor(5000,5000);
 
 		 foreach ($myarray  as  $value)
 		 {
+			  $value;
 		  $value."\n";
-		 // echo $i++;
-			//echo recursive();
-			//$width++;
 			$getimg = getimagesize($value);
 			$x =$getimg[0];
 			$y =$getimg[1];
 			 $source = imagecreatefrompng($value);
-		 //	echo $source;
-		 //	echo $desti.'<br>';
 			$copyimg = 	imagecopy($desti, $source,$i, $u, 0,0,$x,$y);
-		 // var_dump($copyimg);
 			$imgpng = 	imagepng($desti,'./mes_images/sprite.png');
-			 //var_dump($imgpng);
-			//$dest1 =	imagedestroy($desti);//
-			//var_dump($dest1);
-			 //$dest2 = imagedestroy($source);//
-		 //	var_dump($dest2);
-			 $i += $x;
-			 var_dump($x);
-			 var_dump($y);
-			 //echo $u += $y;
-			   //$x."\n";
+			   $i += $x;
 		 }
 
+		 my_generate_css($myarray);
   }
